d401497ef5ea94b689623229890b96de4e6a7852
mormidou <mormidou5606@gmail.com>
2026-01-15 14:10:23 +0100

    update tp01.py

--- tp01.py
+++ tp01.py
@@ -1,1 +1,9 @@
 # Créer une fonction qui renvoie le texte en majuscule
+# Créer une fonction qui renvoie le texte en majuscule
+def texte_en_majuscules(texte):
+    return texte.upper()
+
+# Exemple d'utilisation
+texte = "bonjour tout le monde"
+resultat = texte_en_majuscules(texte)
+print(resultat)  # Affiche : BONJOUR TOUT LE MONDE
